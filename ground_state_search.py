831924fe14fe6b0a5105e456b9864f7befa62c38
alex-k-22 <101887662+alex-k-22@users.noreply.github.com>
2023-04-27 12:40:53 -0400

    Fix a few bugs

--- ground_state_search.py
+++ ground_state_search.py
@@ -13,18 +13,18 @@ from qiskit.quantum_info import Statevector, partial_trace
 
 def get_ground_state(matrix, epsilon):
     # Step 1: Get estimate \theta_0
-    # ef = EigenvalueFinding(matrix, epsilon/2)
+    # ef = EigenvalueFinding(matrix, epsilon/4)
     # theta0 = find_min(ef)
-    theta0 = 0.125  # Skip step 1 for testing purposes just to save time
+    theta0 = 0.2  # Skip step 1 for testing purposes just to save time
 
     # Step 2: Construct the Grover circuit
     ef = EigenvalueFinding(matrix, epsilon/4)  # Need a bit more precision
-    oracle_list = [0]*(2**(ef.qpe_bits + ef.n))
-    good_states = []
+    oracle_list = [0]*(2**(ef.qpe_bits + ef.n))  # The entry at index x is 1 iff x is close to theta_0
+    good_states = []  # List of good states
     for x in range(2**ef.qpe_bits):
         if abs(x/2**ef.qpe_bits - theta0) < epsilon/2:
             for y in range(2**ef.n):
-                z = bin(x)[2:].zfill(ef.qpe_bits) + bin(y)[2:].zfill(ef.n)
+                z = bin(y)[2:].zfill(ef.n) + bin(x)[2:].zfill(ef.qpe_bits)
                 good_states.append(z)
                 oracle_list[int(z, 2)] = 1
 
@@ -34,16 +34,21 @@ def get_ground_state(matrix, epsilon):
     grover = Grover(sampler=Sampler())
 
     # Step 3: Figure out how many iterations are needed
-    qc = grover.construct_circuit(problem=problem, power=1, measurement=False)
+    qc = grover.construct_circuit(problem=problem, power=2, measurement=False)
     # Step 4: Execute circuit and extract statevector
     backend = Aer.get_backend("statevector_simulator")
     # First we need to apply the bit oracle in order to postselect
+    # Recall that a bit oracle is (I\otimes H) (controlled-phase oracle) (I\otimes H), with ctrl = last qubit
     qc.add_register(QuantumRegister(1))
     qc.h(ef.qpe_bits + ef.n)
 
-    po = QuantumCircuit(ef.qpe_bits + ef.n)  # Phase oracle
-    po.diagonal([(-1)**(x in oracle_list) for x in range(2**(ef.qpe_bits + ef.n))], list(range(ef.qpe_bits + ef.n)))
-    cpo = po.to_gate().control(num_ctrl_qubits=1)
+    # Now construct phase oracle (po)
+    po = QuantumCircuit(ef.qpe_bits + ef.n)
+
+    po.diagonal([(-1)**(bin(z)[2:].zfill(ef.qpe_bits + ef.n) in good_states) for z in range(2**(ef.qpe_bits + ef.n))],
+                list(range(ef.qpe_bits + ef.n)))
+
+    cpo = po.to_gate().control(num_ctrl_qubits=1, label="CPO")
     qc.append(cpo, [ef.qpe_bits + ef.n] + list(range(ef.qpe_bits + ef.n)))
 
     qc.h(ef.qpe_bits + ef.n)
@@ -52,22 +57,25 @@ def get_ground_state(matrix, epsilon):
     job = backend.run(qc)
     result = job.result()
     svec = result.get_statevector(qc)
-    projector = Statevector([0, 1]).to_operator()
-    svec.evolve(projector, [ef.qpe_bits+ef.n])  # Postselect last qubit being |1>
-    # Now trace out qubits 0, 1, ..., 8 and 12
-    return partial_trace(svec, list(range(9)) + [12])
+
+    svec = svec.evolve(Statevector([0, 1]).to_operator(), [ef.qpe_bits+ef.n])  # Postselect last qubit being |1>
+    # Now trace out QPE and ancilla qubits
+    return partial_trace(svec, list(range(ef.qpe_bits)) + [ef.qpe_bits + ef.n])
+
 
 if __name__ == "__main__":
     # Specify error and matrix size
     error = 2**(-3)
-    bits_of_precision = ceil(log2(1 / error))
     dim = 2**3
 
     # Choose random Hermitian matrix to run algorithm on by choosing random diagonal matrix and conjugating by unitary
-    d = [0.375]+[0.875 for _ in range(7)]
+    d = [0.2]+[0.7501 for _ in range(dim-1)]
     mat = np.diag(d)
     # un = unitary_group.rvs(dim)
     # mat = un @ np.diag(d) @ un.conj().T
 
     rho = get_ground_state(mat, error)
-    print(rho.probabilities_dict())
+    probs = rho.probabilities()  # Get probabilities
+    probs /= sum(probs)  # Normalize
+    print(probs)
+
