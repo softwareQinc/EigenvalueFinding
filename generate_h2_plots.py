f259e400f849288b56acf5e9fe030d31c8466481
alex-k-22 <101887662+alex-k-22@users.noreply.github.com>
2023-05-30 14:45:08 -0400

    Include code for generating h2 plots

--- generate_h2_plots.py
+++ generate_h2_plots.py
@@ -4,7 +4,7 @@ import tikzplotlib
 
 bond_length_step = 0.1
 min_bond_length = 0.3
-max_bond_length = 2.5
+max_bond_length = 2.5001
 bond_lengths = np.arange(min_bond_length, max_bond_length, bond_length_step)
 
 with open("h2_data.txt", 'r') as f:
@@ -14,8 +14,12 @@ approx = [float(line.split()[0]) for line in lines]
 exact = [complex(line.split()[1]).real for line in lines]
 error = [abs(approx[i] - exact[i]) for i in range(len(approx))]
 
-plt.semilogy(bond_lengths, error)
-plt.show()
+plt.semilogy(bond_lengths, error, base=2)
+plt.semilogy(bond_lengths, [0.01]*len(bond_lengths), base=2)
+plt.xlabel("Bond length")
+plt.ylabel("Error")
+tikzplotlib.save("h2_error_plot.tex")
+# plt.show()
 
 # plt.plot(bond_lengths, approx)
 # plt.plot(bond_lengths, exact)
